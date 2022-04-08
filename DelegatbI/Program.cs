51831dc5479f7fcdf5cc37442a1cbbc72a6dc902
wsr1 <wsr1@K218PC1>
2022-04-08 14:00:03 +0300

    Добавьте файлы проекта.

---
+++ DelegatbI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DelegatbI
+{
+    public delegate string HelloDelegeate(string name);
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"привет, {s}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            HelloDelegeate hD = new HelloDelegeate(someClass.SayHello);
+            Console.WriteLine(hD.Invoke("Antoun"));
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ DelegatbI1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DelegatbI1
+{
+    public delegate void MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate mD = new MyDelegate(delegate { Console.WriteLine("Hello World!"); });
+
+            mD(); //mD.Invoke
+
+            Console.ReadKey();
+        }
+    }
+}
